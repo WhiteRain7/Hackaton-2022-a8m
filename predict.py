56d0027e6ac9a1c20466b3aa09973d605c3a75cd
mrgick <mrgickcool@gmail.com>
2022-08-26 19:23:01 +0300

    bug fix

--- predict.py
+++ predict.py
@@ -9,7 +9,7 @@ device = torch.device("cpu")
 x = torch.FloatTensor([[0.1,0.1,1.],[0.2,0.2,1.],[0.3,0.3,0.3]]) # получить массив массивов
 y = torch.FloatTensor([[1],[1],[0]]) # получить массив массивов # с одним значением в каждом
 
-a = torch.randn(3, 1, device=device, dtype=dtype)
+a = torch.randn(3, 1, device=device, dtype=dtype, requires_grad=True)
 print(a)
 
 learning_rate = 1e-6
@@ -24,6 +24,6 @@ for t in range(2000):
 
     with torch.no_grad():
         a -= learning_rate * a.grad
-        a.grad.zero()
+        a.grad.zero_()
 
 print(f'Result: y_pred = {y_pred}, a = {a}')
