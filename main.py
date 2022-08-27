c695183362c87bba688e7f760d4ef84784b8c93d
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-08-27 14:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,16 @@ def get_time(func):
 
 def main(): # 152 767 ## 14 000 awans
     parsed_data = get_time(csv_parser.parse)(limit = 14000, step = 1)
-    y_pred, layers = get_time(predict.learning)(parsed_data, iterations = 100)
+    y_pred, model = get_time(predict.nn_learning)(parsed_data, iterations = 500, learning_rate = 1e-5)
 
-    print('Start verifying')
+    check_data = get_time(csv_parser.parse)('test.csv', limit = 14000, step = 1)
+    result = predict.predict_all(check_data['X'], model)
+    
+    print('===============================================================\n')
+
+    print('----------------------------------------------------------')
+    print('Start verifying learning...')
+    print('----------------------------------------------------------')
     y_pred = y_pred.t().tolist()[0]
 
     all_ = len(parsed_data['Y'])
@@ -55,5 +62,54 @@ def main(): # 152 767 ## 14 000 awans
     print()
     print('Awans:', awans)
     if awans > 0: print('Verified awans: {}%'.format(round(verified_awans / awans * 100, 2)))
+
+    print()
+    print('----------------------------------------------------------')
+    print('Start verifying prediction...')
+    print('----------------------------------------------------------\n')
+    ys = result.tolist()
+    for i in range(len(ys)): ys[i].append(i) # [[buy-ratio, user_num], ...]
+    ys.sort(reverse = True)
     
+    best_nums = 15
+    print('Best {} positions:'.format(best_nums))
+    for i in range(best_nums):
+        verified = (check_data['Y'][ys[i][1]][0] == max(min(round(ys[i][0]), 1), 0))
+        print('{:>3}) #{:<7} ({:>5}) - {}'.format(i+1, ys[i][1], round(ys[i][0], 2), verified))
+
+    print()
+    worst_nums = 15
+    print('Worst {} positions:'.format(worst_nums))
+    for i in range(-1, -worst_nums-1, -1):
+        verified = (check_data['Y'][ys[i][1]][0] == max(min(round(ys[i][0]), 1), 0))
+        print('{:>3}) #{:<7} ({:>5}) - {}'.format(i, ys[i][1], round(ys[i][0], 2), verified))
+
+    verify_percents = .05
+    top_percent_content = []
+    zeros = 0
+    verified_zeros = 0
+    awans = 0
+    verified_awans = 0
+    
+    for i in range(int(len(ys)*verify_percents)):
+        top_percent_content.append(ys[i])
+        ratio = max(min(round(ys[i][0]), 1), 0)
+        verified = (check_data['Y'][ys[i][1]][0] == ratio)
+            
+        if ratio == 1: awans += 1
+        else: zeros += 1
+        if verified:
+            if ratio == 1: verified_awans += 1
+            else: verified_zeros += 1
+
+    print()
+    print('From {} output values picked {}%, so here\'s a top {}:'.format(len(ys), verify_percents*100, int(len(ys)*verify_percents)))
+    print(' - zeros:', zeros)
+    if zeros != 0: print(' - verified zeros:', verified_zeros / zeros)
+    print()
+    print(' - awans:', awans)
+    if awans != 0: print(' - verified awans:', verified_awans / awans)
+
+    return top_percent_content
+
 main()
