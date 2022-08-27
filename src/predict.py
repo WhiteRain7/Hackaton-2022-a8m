5f637097fa168d02d5b76c7b8196d26a542a8f6e
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-08-27 20:40:06 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,9 @@
 import torch
 
 def learning(parsed, iterations = 50000, learning_rate = 1e-6, min_accuracy = None):
+    '''
+        It's an old "learning" function, you should use "nn_learning" instead.
+    '''
     if (type(parsed) is not dict or
         len(parsed.get('X', [])) == 0 or
         len(parsed.get('Y', [])) == 0):
@@ -80,11 +83,13 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
                         max_renews = 0,         # If NN is learning too slowly, it do restart with new layers. Here you can
                                                 # specify how many restarts available (if 0 - NN will not do any restarts).
                                                 # After finishing, NN will use layer with the lowest loss.
-                        per_iter = 0.3):        # Specify the suitable number of loss decreasing (used with previous args)
+                        per_iter = 0.3,         # Specify the suitable number of loss decreasing (used with previous args)
+                        print_logs = True):
     if (type(parsed) is not dict or
         len(parsed.get('X', [])) == 0 or
         len(parsed.get('Y', [])) == 0):
-            print('Incorrect data to prediction.')
+            if print_logs: print('Incorrect data to learning.')
+            else: raise TypeError('Incorrect data to learning.')
             return
     
 
@@ -104,7 +109,7 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
     optim = torch.optim.SGD(model.parameters(), lr = learning_rate)
 
     iterations_ten_percents = iterations // 10
-    print('Learning NN.')
+    if print_logs: print('Learning NN.')
 
     t = 0
     all_iterations = 0
@@ -121,7 +126,7 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
         loss.backward()
         optim.step()
 
-        if t % iterations_ten_percents == 0: print('Still learning... {}0% ==> {}'.format(t // iterations_ten_percents, loss.item()))
+        if t % iterations_ten_percents == 0 and print_logs: print('Still learning... {}0% ==> {}'.format(t // iterations_ten_percents, loss.item()))
         t += 1
         all_iterations += 1
 
@@ -129,7 +134,7 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
         loss_deltas.pop(0)
         prev_loss = loss.item()
 
-        if t > iterations // 2:
+        if t > iterations // 3:
             if min_accuracy:
                 if loss.item() <= min_accuracy: # if min_accuracy reached
                     if forced_exit: t = iterations+1 # if forced: learning ends
@@ -145,7 +150,7 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
                     loss.item() > (min_accuracy*5   if min_accuracy else 10 ) and sum(loss_deltas) < 1*per_iter): # if learning is very slow and loss is too large
                     max_renews -= 1
 
-                    print('\nToo slowly ({}). Learning restarted. There are {} restarts left.'.format(round(loss_deltas[-1], 2), max_renews))
+                    if print_logs: print('\nToo slowly ({}). Learning restarted. There are {} restarts left.'.format(round(loss_deltas[-1], 2), max_renews))
                     model = get_nn_model(layers_dimensions)
                     if loss.item() < old_model[1].item(): old_model = [model, loss]
                     optim = torch.optim.SGD(model.parameters(), lr = learning_rate)
@@ -154,82 +159,23 @@ def nn_learning(parsed, iterations = 50000,     # Base count of iterations of NN
     
     if old_model[1].item() < loss.item(): model, loss = old_model
     
-    print('Learning finished.\nFinal losses: ' + str(loss.item()) + '\nIterated', all_iterations, 'times.\n')
+    if print_logs: print('Learning finished.\nFinal losses: ' + str(loss.item()) + '\nIterated', all_iterations, 'times.\n')
 
     return y_pred, model
 
 #===========================================================
 
-def sigmoid_learning(parsed, iterations = 50000, learning_rate = 1e-6, min_accuracy = None):
-    if (type(parsed) is not dict or
-        len(parsed.get('X', [])) == 0 or
-        len(parsed.get('Y', [])) == 0):
-            print('Incorrect data to prediction.')
-            return
-
-    import numpy as np
-
-    def sigmoid(x):
-        output = 1/(1+np.exp(-x))
-        return output
-
-    def sigmoid_output_to_derivative(output):
-        return output*(1-output)
-        
-    dtype = torch.float
-    device = torch.device("cpu")
-
-    x = np.array(parsed['X']) # list of lists of input data ==> np_array
-    y = np.array(parsed['Y']) # list of lists of output data ==> np_array
-
-    layers_dimensions = [
-                         len(parsed['X'][0]),
-                         1000,
-                         500,
-                         50,
-                         5,
-                         len(parsed['Y'][0])
-                        ]
-
-    np.random.seed(1)
-    layers = []
-    for i in range(len(layers_dimensions) - 1):
-        layer = 2*np.random.random((layers_dimensions[i], layers_dimensions[i+1])) - 1
-        layers.append(layer)
-
-    iterations_ten_percents = iterations // 10
-    print('Learning NN.')
-    
-    for t in range(iterations):
-        layers_pred = [x]
-        for i in range(len(layers)):
-            layers_pred.append(sigmoid(np.dot(layers_pred[i], layers[i])))
-
-        if t%50000 == 0: print('Still learning... #{}'.format(t))
-
-        layer_error = y - layers[-1].T
-        layer_delta = layer_error * sigmoid_output_to_derivative(layers_pred[-1])
-
-        for i in range(len(layers)-2, -1, -1):
-            layer_error = layer_delta.dot(layers[i].T)
-            layer_delta = layer_error * sigmoid_output_to_derivative(layers_pred[i])
-
-            weight_update = layers[i].dot(layer_delta)
-            layers[i] += learning_rate * weight_update
-    
-    print('Learning finished.\nFinal losses: ' + str(loss.item()) + '\n')
-
-    print('\n=============\n', layers, '\n=============\n')
-
-    return y_pred
-
-#===========================================================
-
 def predict (data):
+    '''
+        Predicts single list of input values.
+    '''
     x = torch.FloatTensor([data]) # list of input data ==> tensor
     return model(x)[0]
 
 def predict_all (data, model):
+    '''
+        Predicts list of lists of input values.
+    '''
     x = torch.FloatTensor(data) # list of lists of input data ==> tensor
     return model(x)
 
