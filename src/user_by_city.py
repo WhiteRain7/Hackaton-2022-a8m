88656eed9d29ec49f6e38779e83d64798e170564
mrgick <mrgickcool@gmail.com>
2022-08-27 16:00:47 +0300

    Make it faster

--- src/user_by_city.py
+++ src/user_by_city.py
@@ -7,14 +7,14 @@ from utils import to_float, read_file_to_list, write_list_to_file
 FILENAME = 'updated_wot.json'
 
 
-def get_percent_by_city(city: str, data: list) -> float:
-    item = list(filter(lambda i, city=city: i['city'] == city, data))
-    if len(item) == 0:
+def get_percent_by_city(city: str, data: dict) -> float:
+    users = data.get(city)
+    if not users:
         return 0
-    return to_float(item[0].get('percent_of_users'))
+    return to_float(users)
 
 
-def load_parced_data() -> list:
+def load_parced_data() -> dict:
     if (not pathlib.Path(FILENAME).is_file()):
         population_data = population.get_data()
         wot_data = wot.get_data()
@@ -32,22 +32,23 @@ def load_parced_data() -> list:
                 max_percent_players = percent_players_in_city
 
         flag = False
+        data = {}
         for i, item in enumerate(wot_data):
             population_in_city = population.get_population_by_city(
                 item['city'], population_data)
             percent_players_in_city = to_float(
                 item.get('users')) / population_in_city
             if (percent_players_in_city < middle_percent_players_in_city):
-                item['percent_of_users'] = (
+                percent_players_in_city = (
                     percent_players_in_city -
                     min_percent_players) / middle_percent_players_in_city * .5
             else:
-                item['percent_of_users'] = (
+                percent_players_in_city = (
                     percent_players_in_city - middle_percent_players_in_city
                 ) / (max_percent_players -
                      middle_percent_players_in_city) * .5 + .5
-            wot_data[i] = item
-        write_list_to_file(FILENAME, wot_data)
+            data.update({item['city']:percent_players_in_city})
+        write_list_to_file(FILENAME, data)
     else:
-        wot_data = read_file_to_list(FILENAME)
-    return wot_data
+        data = read_file_to_list(FILENAME)
+    return data
