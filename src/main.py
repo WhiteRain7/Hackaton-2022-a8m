6685fabb394ca96fee30267142133c9a6c269371
Maxim Makarov <74417675+WhiteRain7@users.noreply.github.com>
2022-08-28 11:01:20 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -46,115 +46,120 @@ def main(get_nn_from = None, # path/to/binary/file or None to learn new nn
 
     if print_logs:
         print('===============================================================\n')
-
+        
         if get_nn_from is None:
-            print('----------------------------------------------------------')
-            print('Start verifying learning...')
-            print('----------------------------------------------------------')
-            y_pred = y_pred.t().tolist()[0]
+                round_border = .5 # if less than round_border, answer is 0, else 1
+                
+                print('----------------------------------------------------------')
+                print('Start verifying learning...')
+                print('----------------------------------------------------------')
+                y_pred = y_pred.t().tolist()[0]
 
-            all_ = len(parsed_data['Y'])
-            verified = 0
+                all_ = len(parsed_data['Y'])
+                verified = 0
 
-            zeros = 0
-            verified_zeros = 0
-            ones = 0
-            verified_awans = 0
+                zeros = 0
+                verified_zeros = 0
+                ones = 0
+                verified_ones = 0
 
-            for i in range(len(parsed_data['Y'])):
-                item_pred = round(y_pred[i], 2)
-                item      = parsed_data['Y'][i][0]
-                items_equals = (round(item_pred) == item)
+                for i in range(len(parsed_data['Y'])):
+                    item_pred    = round(y_pred[i], 2)
+                    item         = parsed_data['Y'][i][0]
+                    items_equals = ((0 if item_pred < round_border else 1) == item)
 
-                if items_equals: verified += 1
+                    if items_equals: verified += 1
 
-                if item == 0:
-                    zeros += 1
-                    if items_equals: verified_zeros += 1
+                    if item == 0:
+                        zeros += 1
+                        if items_equals: verified_zeros += 1
 
-                if item == 1:
-                    ones += 1
-                    if items_equals: verified_awans += 1
+                    if item == 1:
+                        ones += 1
+                        if items_equals: verified_ones += 1
 
-                if len(parsed_data['Y']) < 100:
-                    print('{:>13}'.format('{} - {} - {}'.format(item_pred, item, 'T' if items_equals else 'F')), end = '  ')
-                    if i%5 == 4: print()
+                    if len(parsed_data['Y']) < 100:
+                        print('{:>13}'.format('{} - {} - {}'.format(item_pred, item, 'T' if items_equals else 'F')), end = '  ')
+                        if i%5 == 4: print()
 
-            print()
-            print('All:', all_)
-            print('Verified successfully: {}%'.format(round(verified / all_ * 100, 2)))
-            print()
-            print('Zeros:', zeros)
-            if zeros > 0: print('Verified zeros: {}%'.format(round(verified_zeros / zeros * 100, 2)))
-            print()
-            print('Ones:', ones)
-            if ones > 0: print('Verified ones: {}%'.format(round(verified_awans / ones * 100, 2)))
+                print()
+                print('All:', all_)
+                print('Verified successfully: {}%'.format(round(verified / all_ * 100, 2)))
+                print()
+                print('Zeros:', zeros)
+                if zeros > 0: print('Verified zeros: {}%'.format(round(verified_zeros / zeros * 100, 2)))
+                print()
+                print('Ones:', ones)
+                if ones > 0: print('Verified ones: {}%'.format(round(verified_ones / ones * 100, 2)))
 
-        print()
-        print('----------------------------------------------------------')
-        print('Start verifying prediction...')
-        print('----------------------------------------------------------\n')
-        ys = result.tolist()
-        for i in range(len(ys)): ys[i].append(i) # [[buy-ratio, user_num], ...]
-        ys.sort(reverse = True)
+                print()
+                print('----------------------------------------------------------')
+                print('Start verifying prediction...')
+                print('----------------------------------------------------------\n')
+                ys = result.tolist()
+                for i in range(len(ys)): ys[i].append(i) # [[buy-ratio, user_num], ...]
+                ys.sort(reverse = True)
 
-        best_nums = 15
-        print('Best {} positions:'.format(best_nums))
-        for i in range(best_nums):
-            verified = (check_data['Y'][ys[i][1]][0] == max(min(round(ys[i][0]), 1), 0))
-            print('{:>3}) #{:<7} ({:>5}) - {}'.format(i+1, ys[i][1], round(ys[i][0], 2), verified))
+                # if less than round_border, answer is 0, else 1
+                round_border = ys[-1][0] + (ys[0][0] - ys[-1][0]) / (ones / (1 if zeros == 0 else zeros) + 1)
+                print(round_border)
+                
+                best_nums = 15
+                print('Best {} positions:'.format(best_nums))
+                for i in range(best_nums):
+                    verified = (check_data['Y'][ys[i][1]][0] == (0 if ys[i][0] < round_border else 1))
+                    print('{:>3}) #{:<7} ({:>5}) - {}'.format(i+1, ys[i][1], round(ys[i][0], 2), verified))
 
-        print()
-        median_nums = 15
-        print('Median {} positions:'.format(median_nums))
-        for i in range(len(ys)//2 - median_nums//2, len(ys)//2 + median_nums//2 + 1):
-            verified = (check_data['Y'][ys[i][1]][0] == max(min(round(ys[i][0]), 1), 0))
-            print('{:>3}) #{:<7} ({:>5}) - {}'.format(i+1, ys[i][1], round(ys[i][0], 2), verified))
+                print()
+                median_nums = 15
+                print('Median {} positions:'.format(median_nums))
+                for i in range(len(ys)//2 - median_nums//2, len(ys)//2 + median_nums//2 + 1):
+                    verified = (check_data['Y'][ys[i][1]][0] == (0 if ys[i][0] < round_border else 1))
+                    print('{:>3}) #{:<7} ({:>5}) - {}'.format(i+1, ys[i][1], round(ys[i][0], 2), verified))
 
-        print()
-        worst_nums = 15
-        print('Worst {} positions:'.format(worst_nums))
-        for i in range(-1, -worst_nums-1, -1):
-            verified = (check_data['Y'][ys[i][1]][0] == max(min(round(ys[i][0]), 1), 0))
-            print('{:>3}) #{:<7} ({:>5}) - {}'.format(i, ys[i][1], round(ys[i][0], 2), verified))
+                print()
+                worst_nums = 15
+                print('Worst {} positions:'.format(worst_nums))
+                for i in range(-1, -worst_nums-1, -1):
+                    verified = (check_data['Y'][ys[i][1]][0] == (0 if ys[i][0] < round_border else 1))
+                    print('{:>3}) #{:<7} ({:>5}) - {}'.format(i, ys[i][1], round(ys[i][0], 2), verified))
+                
+                zeros = 0
+                ones = 0
+                for i in range(len(ys)):
+                    if check_data['Y'][ys[i][1]][0] == 0: zeros += 1
+                    else: ones += 1
 
-        zeros = 0
-        ones = 0
-        for i in range(len(ys)):
-            if check_data['Y'][ys[i][1]][0] == 0: zeros += 1
-            else: ones += 1
+                print()
+                print('Here\'s in test file:')
+                print('Zeros:', zeros)
+                print('Ones:', ones)
 
-        print()
-        print('Here\'s:')
-        print('Zeros:', zeros)
-        print()
-        print('Ones:', ones)
+                verify_percents = .05
+                top_percent_content = []
+                zeros = 0
+                verified_zeros = 0
+                ones = 0
+                verified_ones = 0
+                
+                for i in range(int(len(ys)*verify_percents)):
+                    top_percent_content.append([ys[i][1], (0 if ys[i][0] < round_border else 1)])
+                    ratio = (0 if ys[i][0] < round_border else 1)
+                    verified = (check_data['Y'][ys[i][1]][0] == ratio)
+                        
+                    if ratio == 1: ones += 1
+                    else: zeros += 1
+                    if verified:
+                        if ratio == 1: verified_ones += 1
+                        else: verified_zeros += 1
 
-        verify_percents = .05
-        top_percent_content = []
-        zeros = 0
-        verified_zeros = 0
-        ones = 0
-        verified_awans = 0
-
-        for i in range(int(len(ys)*verify_percents)):
-            top_percent_content.append(ys[i][1])
-            ratio = max(min(round(ys[i][0]), 1), 0)
-            verified = (check_data['Y'][ys[i][1]][0] == ratio)
-
-            if ratio == 1: ones += 1
-            else: zeros += 1
-            if verified:
-                if ratio == 1: verified_awans += 1
-                else: verified_zeros += 1
-
-        print()
-        print('From {} output values picked {}%, so here\'s a top {}:'.format(len(ys), verify_percents*100, int(len(ys)*verify_percents)))
-        print('Zeros:', zeros)
-        if zeros > 0: print('Verified zeros: {}%'.format(round(verified_zeros / zeros * 100, 2)))
-        print()
-        print('Ones:', ones)
-        if ones > 0: print('Verified ones: {}%'.format(round(verified_awans / ones * 100, 2)))
+                print()
+                print('From {} output values picked {}%, so here\'s a top {}:'.format(len(ys), verify_percents*100, int(len(ys)*verify_percents)))
+                print('Zeros:', zeros)
+                if zeros > 0: print('Verified zeros: {}%'.format(round(verified_zeros / zeros * 100, 2)))
+                print()
+                print('Ones:', ones)
+                if ones > 0: print('Verified ones: {}%'.format(round(verified_ones / ones * 100, 2)))
 
     return top_percent_content
 
